0824e0dcb7b73bb33995986447a402bb465b2e9a
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-07-04 23:33:10 +0700

    feat: add routes and navlink

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
 import TopBar from "./components/Topbar";
@@ -12,7 +13,14 @@ function App() {
           <Sidebar/>
           <div className="flex-1 flex flex-col bg-custom-primary">
             <TopBar/>
-            <Home/>
+
+            {/* ROUTES */}
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              {/* <Route path="/albums" element={<Albums/>} />
+              <Route path="/songs" element={<Songs/>} />
+              <Route path="/artists" element={<Artists/>} /> */}
+            </Routes>
           </div>
       </div>
     </>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 // ICONS
 import homeActive from "../assets/icons/homeActive.png";
@@ -12,12 +13,7 @@ import artistDefault from "../assets/icons/ArtistDefault.png";
 import { TbLayoutSidebarLeftCollapse, TbLayoutSidebarLeftExpand } from "react-icons/tb";
 
 const Sidebar = () => {
-    const [activeItem, setActiveItem] = useState("Home");
     const [open, setOpen] = useState(true);
-
-    const handleItemClick = (itemName) => {
-        setActiveItem(itemName);
-    }
 
     return(
         <section id="sideBar" className={`${open ? "w-72 p-5" : "w-36 p-10"} w-2xs bg-custom-secondary h-screen relative ease-in-out p-14 pt-10 transition-all duration-300`}>
@@ -32,84 +28,57 @@ const Sidebar = () => {
           <h1 className={`text-1xl h-14 text-font-color1 ease-in-out duration-300`}>Menu</h1>
             <ul className="p-0">
                 <li className="list-none block h-10 rounded-md mb-2 relative">
-                    <a
-                        href="/"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            handleItemClick("Home");}}
-                        className={`flex items-center h-full rounded-md transition-all duration-200
-                            ${activeItem === "Home" ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}
-                    >
-                        <div className="relative flex items-center w-full gap-3">
-                            <img src={activeItem === "Home" ? homeActive : homeDefault} className="w-5 h-5" alt="home"/>
-                            <span className={`${!open && "hidden"}`}>Home</span>
-                            {activeItem === "Home" && open && (
-                                <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
-                            )}
-                        </div>
-                    </a>
+                    <NavLink to={"/"} className= {({ isActive }) => `flex items-center h-full rounded-md transition-all duration-200 ${isActive ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}>
+                        {({ isActive }) => (
+                            <div className="relative flex items-center w-full gap-3">
+                                <img src={isActive ? homeActive : homeDefault} className="w-5 h-5" alt="home"/>
+                                <span className={`${!open && "hidden"}`}>Home</span>
+                                {isActive && open && (
+                                    <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
+                                )}
+                            </div>
+                        )}
+                    </NavLink>
                 </li>
 
                 <li className="list-none block h-10 rounded-md mb-2 relative">
-                    <a 
-                        href="/"
-                        onClick={(e) =>{
-                            e.preventDefault();
-                            handleItemClick("Albums");}}
-                        className={`flex items-center h-full rounded-md transition-all duration-200
-                            ${open ? "justify-start" : "justify-center"}
-                            ${activeItem === "Albums" ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}
-                        >
-                        <div className="relative flex items-center w-full gap-3">
-                            <img src={activeItem === "Albums" ? albumsActive : albumsDefault} className="w-5 h-5" alt="albums"/>
-                            <span className={`${!open && "hidden"}`}>Albums</span>
-                            {activeItem === "Albums" && open && (
-                                <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
-                            )}
-                        </div>
-                    </a>
+                    <NavLink to={"/albums"} className= {({ isActive }) => `flex items-center h-full rounded-md transition-all duration-200 ${isActive ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}>
+                        {({ isActive }) => (
+                            <div className="relative flex items-center w-full gap-3">
+                                <img src={isActive ? albumsActive : albumsDefault} className="w-5 h-5" alt="albums"/>
+                                <span className={`${!open && "hidden"}`}>Albums</span>
+                                {isActive && open && (
+                                    <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
+                                )}
+                            </div>
+                        )}
+                    </NavLink>
                 </li>
                 <li className="list-none block h-10 rounded-md mb-2 relative">
-                    <a 
-                        href="/"
-                        onClick={(e) =>{
-                            e.preventDefault();
-                            handleItemClick("Song");
-                        }}
-                        className={`flex items-center h-full rounded-md transition-all duration-200
-                            ${open ? "justify-start" : "justify-center"}
-                            ${activeItem === "Song" ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}
-                            `}
-                        >
-                        <div className="relative flex items-center w-full gap-3">
-                            <img src={activeItem === "Song" ? songActive : songDefault} className="w-5 h-5" alt="song"/>
-                            <span className={`${!open && "hidden"}`}>Song</span>
-                            {activeItem === "Song" && open && (
-                                <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
-                            )}
-                        </div>
-                    </a>
+                    <NavLink to={"/songs"} className= {({ isActive }) => `flex items-center h-full rounded-md transition-all duration-200 ${isActive ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}>
+                        {({ isActive }) => (
+                            <div className="relative flex items-center w-full gap-3">
+                                <img src={isActive ? songActive : songDefault} className="w-5 h-5" alt="song"/>
+                                <span className={`${!open && "hidden"}`}>Song</span>
+                                {isActive && open && (
+                                    <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
+                                )}
+                            </div>
+                        )}
+                    </NavLink>
                 </li>
                     <li className="list-none block h-10 rounded-md mb-2 relative">
-                    <a 
-                        href="/"
-                        onClick={(e) =>{
-                            e.preventDefault();
-                            handleItemClick("Artist");
-                        }}
-                        className={`flex items-center h-full rounded-md transition-all duration-200
-                            ${open ? "justify-start" : "justify-center"}
-                            ${activeItem === "Artist" ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}
-                            `}
-                        >
-                        <div className="relative flex items-center w-full gap-3">
-                            <img src={activeItem === "Artist" ? artistActive : artistDefault} className="w-5 h-5" alt="artist"/>
-                            <span className={`${!open && "hidden"}`}>Artist</span>
-                            {activeItem === "Artist" && open && (
-                                <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
-                            )}
-                        </div>
-                    </a>
+                    <NavLink to={"/artists"} className= {({ isActive }) => `flex items-center h-full rounded-md transition-all duration-200 ${isActive ? "text-primary font-semibold" : "text-font-color2 hover:text-font-color1"}`}>
+                        {({ isActive }) => (
+                            <div className="relative flex items-center w-full gap-3">
+                                <img src={isActive ? artistActive : artistDefault} className="w-5 h-5" alt="artist"/>
+                                <span className={`${!open && "hidden"}`}>Artist</span>
+                                {isActive && open && (
+                                    <div className={`absolute top-0 bottom-0 right-57 w-1 rounded-e-lg bg-third transition-all duration-200 ${!open && "left-0"}`}></div>
+                                )}
+                            </div>
+                        )}
+                    </NavLink>
                 </li>
             </ul>
         </section>
